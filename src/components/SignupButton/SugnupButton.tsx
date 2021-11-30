70c6ce680e78cbce95daa7d7e1a949ccd459b3e3
Stackbit Code Editor <projects@stackbit.com>
2021-11-30 14:25:00 +0000

    SugnupButton.tsx: changed

--- src/components/SignupButton/SugnupButton.tsx
+++ src/components/SignupButton/SugnupButton.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import classNames from 'classnames';
 import Link from '@stackbit/components/dist/utils/link';
-import ArrowLeft from '../../svgs/arrow-left';
+import ArrowLeft from '@stackbit/components/dist/svgs/arrow-left';
 import ArrowRight from '../../svgs/arrow-right';
 import Cart from '../../svgs/cart';
 import Facebook from '../../svgs/facebook';
