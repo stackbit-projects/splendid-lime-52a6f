067963bd00fd3f1897282ba8d56b3c6ae1593b24
Stackbit Code Editor <projects@stackbit.com>
2021-11-30 14:56:42 +0000

    register-components.js: changed

--- src/components/register-components.js
+++ src/components/register-components.js
@@ -1,12 +1,10 @@
 import dynamic from 'next/dynamic';
 import { registerComponents } from '@stackbit/components';
 import { componentsMap } from '@stackbit/components/dist/components-map';
-import SignupButton from './SignupButton';
 
 registerComponents({
     // Register all Stackbit components
-    ...componentsMap,
-    Action: SignupButton
+    ...componentsMap
 
     // Override any static or dynamic component,
     // or register your own dynamic component.
