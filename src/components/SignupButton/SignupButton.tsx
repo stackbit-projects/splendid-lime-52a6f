9ed2515b7af44a3ee286cb146eedcb2912ccc034
Stackbit Code Editor <projects@stackbit.com>
2021-11-30 14:36:41 +0000

    SignupButton.tsx: changed

--- src/components/SignupButton/SignupButton.tsx
+++ src/components/SignupButton/SignupButton.tsx
@@ -44,6 +44,7 @@ export default function Action(props) {
         <button
             aria-label={altText}
             id={cssId}
+            onC
             className={classNames('sb-component', 'sb-component-block', style === 'link' ? 'sb-component-link' : 'sb-component-button', cssClasses, {
                 'sb-component-button-primary': style === 'primary',
                 'sb-component-button-secondary': style === 'secondary'
