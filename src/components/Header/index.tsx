7c151f5005f73f5737dbe549caac905345951ea2
Stackbit Code Editor <projects@stackbit.com>
2021-11-30 20:51:17 +0000

    index.tsx: changed

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -217,7 +217,6 @@ function listOfLinks(links, inMobileMenu = false) {
     return links.map((link, index) => {
         console.log(link);
         const Action = getComponent(link.type === 'Button' ? 'Button' : 'Action');
-
         const defaultStyle = link.type === 'Link' ? 'link' : 'secondary';
         const style = link.style || defaultStyle;
         return (
