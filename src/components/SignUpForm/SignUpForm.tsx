0876b40e4f7b8215835ab28b9e6541e6319283ed
Stackbit Code Editor <projects@stackbit.com>
2021-11-30 14:45:50 +0000

    SignUpForm.tsx: changed

--- src/components/SignUpForm/SignUpForm.tsx
+++ src/components/SignUpForm/SignUpForm.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import SignupButton from '../SignupButton/SignupButton';
 
-export const SignUpForm = () => {
+export default const SignUpForm = () => {
     const [opened, setOpened] = React.useState(false);
 
     if (opened) {
